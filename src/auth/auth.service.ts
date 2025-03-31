e103020e4431e034100aa5ec4259006af63de7ad
jasim-binumar <jasimbinumar@gmail.com>
2025-03-31 23:16:31 +0500

    initial commit contains setting up the project by setting up docker, prisma etc.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable ({})
+export class AuthService {
+
+    signUp(){
+        return {msg: 'I am signed Up'}
+    }
+
+    signIn(){
+        return {msg: 'I am signed In'}
+    }
+
+}
